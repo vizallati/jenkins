be877598df93c299a45403bb222c3702b5e744bb
Ekwoge Ian <iekwoge@yahoo.com>
2023-03-30 14:17:30 +0100

    Add pipeline script to clone remote git repo

---
+++ pullRemoteRepo.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+
+    parameters {
+        string(name: 'GIT_URL', defaultValue: 'https://github.com/vizallati/allati.git', description: 'URL of the Git repository to clone')
+        string(name: 'GIT_BRANCH', defaultValue: 'main', description: 'Branch of the Git repository to clone')
+        string(name: 'TARGET_DIR', defaultValue: 'C:/Users/iekwo/Desktop/Github/repo', description: 'Target directory to clone the Git repository to')
+    }
+
+    stages {
+        stage('Clone Repository') {
+            steps {
+                dir(TARGET_DIR) {
+                    git branch: GIT_BRANCH, url: GIT_URL
+                }
+            }
+        }
+    }
+}
